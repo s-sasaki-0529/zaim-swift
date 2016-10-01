7aaca8c39abe5e5d540f4abb0695c5b8934f03a0
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-01 09:30:42 +0900

    Initial Commit

---
+++ zaimInputter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zaimInputter
+//
+//  Created by 笹木信吾 on 2016/10/01.
+//  Copyright © 2016年 笹木信吾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
