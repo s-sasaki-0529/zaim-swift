ee26f002aa488e266dbfe4249a7f2c701e4f4100
笹木信吾 <shingo.sasaki.0529@gmail.com>
2016-10-01 13:31:27 +0900

    Q2からQ3への画面遷移

--- zaimInputter/Zaim.swift
+++ zaimInputter/Zaim.swift
@@ -11,12 +11,23 @@ import Foundation
 class Zaim {
   
   var place: String = "";
-  var genre: String = "";
   var amount: Int = 0;
   var comment: String = "";
+  var genre: String = "";
   
   init () {
     
+  }
+  
+  internal func genreToID () -> String {
+    let genreToID = [
+      "食料品": "10101" ,
+      "朝ご飯": "10103" ,
+      "昼ご飯": "10104" ,
+      "晩ご飯": "10105" ,
+      "消耗品": "10201"
+    ]
+    return genreToID[self.genre]!
   }
   
 }
