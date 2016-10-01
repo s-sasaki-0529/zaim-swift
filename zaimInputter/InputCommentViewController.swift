3ee8ba34b737e7ef8867e825dc5627d62260dee2
笹木信吾 <shingo.sasaki.0529@gmail.com>
2016-10-01 23:45:28 +0900

    詳細入力画面にて、これまでの入力内容を表示する

--- zaimInputter/InputCommentViewController.swift
+++ zaimInputter/InputCommentViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 class InputCommentViewController: UIViewController {
   private let zaim: Zaim = (UIApplication.sharedApplication().delegate as! AppDelegate).zaim
-  
+  @IBOutlet weak var placeLabel: UILabel!
+  @IBOutlet weak var genreLabel: UILabel!
+  @IBOutlet weak var amountLabel: UILabel!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    placeLabel.text = zaim.place
+    genreLabel.text = zaim.genre
+    amountLabel.text = "¥" + String(zaim.amount)
   }
   
+  /* 登録 */
+  @IBAction func onTappedSubmitButton() {
+    
+  }
+  
+  /* 戻る */
   @IBAction func onTappedBackButton() {
     self.dismissViewControllerAnimated(true, completion: nil)
   }
