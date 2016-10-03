ef021cdcd4fe48c48db7d1db9c20f52a8a11a120
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-03 17:36:08 +0900

    各ファイルのヘッダーコメントを削除

--- zaimInputter/InputCommentViewController.swift
+++ zaimInputter/InputCommentViewController.swift
@@ -1,11 +1,3 @@
-//
-//  InputCommentViewController.swift
-//  zaimInputter
-//
-//  Created by 笹木信吾 on 2016/10/01.
-//  Copyright © 2016年 笹木信吾. All rights reserved.
-//
-
 import UIKit
 import AudioToolbox
 class InputCommentViewController: UIViewController , UITextFieldDelegate {
--- zaimInputter/Zaim.swift
+++ zaimInputter/Zaim.swift
@@ -1,11 +1,3 @@
-//
-//  Zaim.swift
-//  zaimInputter
-//
-//  Created by 笹木信吾 on 2016/10/01.
-//  Copyright © 2016年 笹木信吾. All rights reserved.
-//
-
 import Foundation
 import OAuthSwift
 
