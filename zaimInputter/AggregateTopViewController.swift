14db1343dd655eb334e5df5263d517886cbdb973
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-04 21:21:34 +0900

    集計情報画面のデータの雛形を作成

--- zaimInputter/AggregateTopViewController.swift
+++ zaimInputter/AggregateTopViewController.swift
@@ -9,6 +9,44 @@
 import UIKit
 class AggregateTopViewController: UIViewController , UITableViewDelegate {
   
+  let contents = [
+    [
+      "title": "基本情報" ,
+      "rows" : [
+        "入力回数" ,
+        "総収入" ,
+        "総支出" ,
+        "総利益"
+      ]
+    ] ,
+    [
+      "title": "日別集計" ,
+      "rows": [
+        "累計"
+      ]
+    ] ,
+    [
+      "title": "月別集計" ,
+      "rows": [
+        "累計" ,
+        "食費" ,
+        "ガス代" ,
+        "電気代" ,
+        "水道代" ,
+        "ポケモンGO" ,
+        "デグー関連"
+      ]
+    ] ,
+    [
+      "title": "ランキング" ,
+      "rows": [
+        "カテゴリ" ,
+        "ジャンル" ,
+        "支払先"
+      ]
+    ]
+  ]
+  
   /* 戻る */
   @IBAction func onTappedBackButton() {
     self.dismissViewControllerAnimated(true, completion: nil)
