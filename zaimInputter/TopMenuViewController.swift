52a54ae39a92cd90a2f3c7d1aaedfb2089f317ed
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-04 19:41:43 +0900

    入力するか集計を見るかを選べるトップ画面を作成

---
+++ zaimInputter/TopMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TopMenuViewController.swift
+//  zaimInputter
+//
+//  Created by 笹木信吾 on 2016/10/04.
+//  Copyright © 2016年 笹木信吾. All rights reserved.
+//
+
+import UIKit
+class TopMenuViewController: UIViewController {
+  
+  /* 入力する */
+  @IBAction func onTappedInputButton() {
+    self.performSegueWithIdentifier("inputplace", sender: self)
+  }
+  
+}
